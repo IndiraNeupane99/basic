d262d2f60af61374ed1132114603c0ac8302ce2f
IndiraNeupane99 <indiraneupane260@gmail.com>
2025-05-18 14:25:10 +0545

    first commit

---
+++ js/resp.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector('.burger');
+const navbar = document.querySelector('.navbar');
+const navList = document.querySelector('.nav-list');
+const rightNav = document.querySelector('.rightNav');
+
+burger.addEventListener('click', () => {
+    navList.classList.toggle('v-class-resp');
+    rightNav.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+});
